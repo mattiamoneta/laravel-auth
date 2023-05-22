aff7722123e8a0e090d17924a1cd72d53896797f
mattiamoneta <in.mattiamoneta@gmail.com>
2023-05-22 20:54:42 +0200

    Final + BONUS

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -28,15 +28,20 @@
                                     <td>{{ $project->description }}</td>
                                     <td>{{ $project->thumb }}</td>
                                     <td>
-                                        <a class="btn btn-primary"
-                                            href="{{ route('admin.projects.show', $project->id) }}"><i
-                                                class="fa-solid fa-info"></i></a>
-                                        <a class="btn btn-primary"
-                                            href="{{ route('admin.projects.edit', $project->id) }}"><i
-                                                class="fa-solid fa-file-pen"></i></a>
-                                        <a class="btn btn-danger"
-                                            href="{{ route('admin.projects.destroy', $project->id) }}"><i
-                                                class="fa-solid fa-trash-can"></i></a>
+                                        <form action="{{ route('admin.projects.destroy', $project->id) }}" method="post">
+                                            <a class="btn btn-primary"
+                                                href="{{ route('admin.projects.show', $project->id) }}"><i
+                                                    class="fa-solid fa-info"></i></a>
+                                            <a class="btn btn-primary"
+                                                href="{{ route('admin.projects.edit', $project->id) }}"><i
+                                                    class="fa-solid fa-file-pen"></i></a>
+
+                                            @csrf
+                                            @method('DELETE')
+                                            <button type="submit" class="btn btn-danger"
+                                                onclick="return confirm('Are you sure to delete?');"><i
+                                                    class="fa-solid fa-trash-can"></i></button>
+                                        </form>
                                     </td>
                                 </tr>
                             @endforeach
